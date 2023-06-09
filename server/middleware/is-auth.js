0b1ba3b538fef9ae566412f61aa0e35a7911a4ea
mohamed271220 <mohamedaprahym@gmail.com>
2023-06-09 12:50:22 +0300

    getting started I

---
+++ server/middleware/is-auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    const authHeader = req.get("Authorization");
+    if(!authHeader){
+        const error = new Error("Not authenticated");
+        error.statusCode=401
+        throw error
+    }
+  const token = req.get("Authorization").split(" ")[1];
+  let decodedToken;
+  try {
+    // verify the token and decode it
+    // same secret used to create the token
+    decodedToken = jwt.verify(token, "TheSecretOfTurningAZeroIntoAnOneIs");
+  } catch (err) {
+    err.statusCode = 500;
+    throw err;
+  }
+  // if token is undefined
+  if (!decodedToken) {
+    const error = new Error("Not authenticated");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  req.userId = decodedToken.userId;
+  next();
+};
